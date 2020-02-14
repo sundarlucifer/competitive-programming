bfc0b1517eb5400f963de7ecfa242cdb44c0376b
sundarlucifer <sundarlucifer@gmail.com>
2020-02-14 23:47:58 +0530

    push from mac

--- datastructures-and-algorithms/linkedlist/5_sumLists.cpp
+++ datastructures-and-algorithms/linkedlist/5_sumLists.cpp
@@ -13,26 +13,12 @@ Node* newNode(int val){
 	return node;
 }
 
-
 Node* insert(Node* head, int val){
 	if(head==NULL) return newNode(val);
 	Node* tmp = head;
 	while(tmp->next!=NULL) tmp = tmp->next;
 	tmp->next = newNode(val);
 	return head;
-}
-
-Node* reverseLL(Node* head){
-	Node* prev = NULL;
-	Node* curr = head;
-	Node* next = curr;
-	while(next!=NULL){
-		next = curr->next;
-		curr->next = prev;
-		prev = curr;
-		curr = next;
-	}
-	return prev;
 }
 
 void printList(Node* head){
@@ -50,12 +36,22 @@ Node* sumOf2Numbers(Node* list1, Node* list2){
 	Node* ptr2 = list2;
 	int carry=0, sum;
 	while(ptr1!=NULL && ptr2!=NULL){
-		cout << "carry : " << carry << endl;
-		sum =  (ptr1->data + ptr2->data) % 10;
-		list3 = insert(list3, sum + carry);
-		carry = (ptr1->data + ptr2->data);
-		carry = (carry>9) ? 1 : 0;
+		sum =  (carry + ptr1->data + ptr2->data);
+		list3 = insert(list3, sum%10);
+		carry = sum > 9 ? 1 : 0;
+        ptr1 = ptr1->next;
+        ptr2 = ptr2->next;
 	}
+    Node* ptr = (ptr1 == NULL) ? ptr2 : ptr1;
+    while(ptr!=NULL){
+        sum =  (carry + ptr->data);
+        list3 = insert(list3, sum%10);
+        carry = sum > 9 ? 1 : 0;
+        ptr = ptr->next;
+    }
+    if(carry > 0)
+        list3 = insert(list3, carry);
+    
 	return list3;
 }
 
@@ -64,12 +60,6 @@ int main(){
 	Node* list1 = NULL;
 	Node* list2 = NULL;
 	int val = 1;
-	//while(1){
-	//	cout << "Enter num (-1 to quit): ";
-	//	cin >> val;
-	//	if(val==-1) break;
-	//	list = insert(list, val);
-	//}
 	
 	int num1, num2;
 	cin >> num1 >> num2;
@@ -77,22 +67,24 @@ int main(){
 		list1 = insert(list1, num1%10);
 		num1 /= 10;
 	}
+    // if num1 was 0 initially
 	if(list1 == NULL) list1 = insert(list1, 0);
 	
 	while(num2>0){
 		list2 = insert(list2, num2%10);
 		num2 /= 10;
 	}
+    // if num2 was 0 initially
 	if(list2 == NULL) list2 = insert(list2, 0);
 	
-	list1 = reverseLL(list1);
-	list2 = reverseLL(list2);
-	
+    // one's place digit is in head node
+    // i.e number is represented in reverse
 	printList(list1);
 	printList(list2);
 	
 	Node* sumList = sumOf2Numbers(list1, list2);
 	
+    // reverse of the sum
 	printList(sumList);
 	
 	return 0;
